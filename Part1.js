23c6304d0a774b4b130ca89eec59e0a5415b851a
Szymek <szymon.rzadca@gmail.com>
2018-08-07 16:09:05 +0200

    adding an enter key event, which changes the color of the header

--- Part1.js
+++ Part1.js
@@ -6,6 +6,12 @@ $('li').click(function(){
   console.log('any li was clicked');
 })
 
-$('input').eq(0).keypress(function(){
-  $('h3').toggleClass('turnBlue');
+// $('input').eq(0).keypress(function(){
+//   $('h3').toggleClass('turnBlue');
+// })
+
+$('input').eq(0).keypress(function(event){
+  if (event.which === 13) {
+    $('h3').toggleClass('turnBlue')
+  }
 })
