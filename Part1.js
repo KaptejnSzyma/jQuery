9ac1da3caffe120825b443dd76ef648bb6ef5d22
Szymek <szymon.rzadca@gmail.com>
2018-08-07 16:12:12 +0200

    basic jQuery animation example

--- Part1.js
+++ Part1.js
@@ -19,3 +19,7 @@ $('input').eq(0).keypress(function(event){
 $('h1').on('mouseenter', function(){
   $(this).toggleClass('turnBlue')
 })
+
+$('input').eq(1).on('click', function(){
+  $('.container').fadeOut(3000)
+})
